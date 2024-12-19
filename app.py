91d9fab1f33527b813abeb159cfbe46b1058fc3c
carlos mackenzie <mackenzie.carlos@gmail.com>
2024-12-18 23:16:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.title("Modelo de Análisis de Riesgo de Crédito FEMAP")
 
-salario = st.number_input('Salario Mensual',value=3500000,format='{:,d}')
+salario = st.number_input('Salario Mensual',value=3500000,format=':,d')
 edad = st.text_input('Edad', value=25, type="default", label_visibility="visible")
 ningreso = st.slider('Nivel de Ingreso',1,6,12)
 antiguedad = st.slider('Antiguedad',0,4,20)
-monto = st.number_input('Monto del Crédiro', value=5000000, label_visibility="visible",format='{:,d}')
+monto = st.number_input('Monto del Crédiro', value=5000000, label_visibility="visible",format=':,d')
 cdesc = st.number_input('Monto del Crédiro', value=0.25, label_visibility="visible",format="%.2f")
