084df68522da16e892ffce4245a2399b18a93205
carlos mackenzie <mackenzie.carlos@gmail.com>
2024-12-21 22:36:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 import streamlit as st
 
 st.title("Modelo de Análisis de Riesgo de Crédito FEMAP")
-
-salario = st.number_input('Salario Mensual',value=3500000)
-st.write("{:0,.2f}".format(float(salario)))
-
+st.divider()
+salario = st.number_input('Salario Mensual')
+st.write("{:0,.0f}".format(float(salario)))
 edad = st.text_input('Edad', value=25, type="default", label_visibility="visible")
-ningreso = st.slider('Nivel de Ingreso',1,12,6)
-antiguedad = st.slider('Antiguedad',0,20,4)
-monto = st.number_input('Monto del Crédito', value=5000000, label_visibility="visible")
-st.write("{:0,.2f}".format(float(monto)))
-cdesc = st.number_input('Capacidad de Descuento', value=0.25, label_visibility="visible",format="%0.1f")
+plazo = st.slider('Plazo en Meses',0,180,12)
+gasto_financiero = st.number_input('Gasto Financiero')
+st.write("{:0,.0f}".format(float(gasto_financiero)))
+st.markdown('''
+    :red[Gasto Financiero:] :black[Valor de todas las cuotas de los créditos activos inluyendo la futura cuota del nuevo crédito de ser aprobado]''')
+cap_des = st.slider('Capacidad de Descuento %',0,50,25)
+capital = st.number_input('Monto del Credito',label_visibility="visible")
+st.write("{:0,.0f}".format(float(capital)))
+ind_cap = capital/salario
+empresa = st.selectbox('Mapfre Generales','Mapfre Vida','Andiasistencia','Cesvicolombia','FEMAP','Solunion','Otros')
+municipio = st.selectbox('Bogota','Medellin','Cali','Otros')
+destino = st.selectbox('Polizas e Impuestos','Promocion','Extraordinario','Prima',
+                       'Recreacion','Compra de Cartera','otros')
+garantia = st.radio("Tipo de Garantia",
+    ["Real", "***Personal***", "Documentary :movie_camera:"],
+    captions=[
+        "Laugh out loud.",
+        "Get the popcorn.",
+        "Never stop learning.",
+    ],
+)
