49d8de699bd63a55c5a3325246f257029a6b4695
carlos mackenzie <mackenzie.carlos@gmail.com>
2024-12-21 23:59:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import pickle
 
 st.title("Modelo de Análisis de Riesgo de Crédito FEMAP")
 st.divider()
@@ -17,8 +18,30 @@ empresa = st.selectbox("Empresa",('Mapfre Generales','Mapfre Vida','Andiasistenc
 municipio = st.selectbox("Municipio",('Bogota','Medellin','Cali','Otros'))
 destino = st.selectbox("Destino",('Polizas e Impuestos','Promocion','Extraordinario','Prima',
                        'Recreacion','Compra de Cartera','otros'))
-garantia = st.radio("Tipo de Garantia",
-    ["Real", "Personal"])
+if (municipio='Bogota'):
+  mun_index=0
+if (municipio='Medellin'):
+  mun_index=1
+if (municipio='Cali'):
+  mun_index=2
+if (municipio='Otros'):
+  mun_index=30  
+if (empresa='Mapfre Generales'):
+  emp_index=0
+if (empresa='Mapfre Vida'):
+  emp_index=1
+if (empresa='Andiasistencia'):
+  emp_index=2
+if (empresa='Cesvicolombia'):
+  emp_index=3
+if (empresa='FEMAP'):
+  emp_index=4
+if (empresa='Solunion'):
+  emp_index=5
+if (empresa='Otros'):
+  emp_index=7  
+garantia = st.radio("Tipo de Garantia",["Real", "Personal"])
 if st.button("Calcular Riesgo de Crédito"):
-    ind_cap = capital/salario
-    st.write("{:0,.2f}".format(float(ind_cap)))
+  ind_cap = capital/salario
+  st.write("{:0,.2f}".format(float(ind_cap)))
+  vp=[[cap_des,salario,mun_index,plazo,gasto_financiero,emp_index,NOMBREDEST_index,EDAD,ANTIGUEDAD,IND_CAP,TIPOGARANT_index,CAPITALINI,NOMBREGARA_index]]
